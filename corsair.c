622ce6355bef8d1819dd6a5a8b0642247c4da1ef
Rafael Z Tanganelli <kabelu@gmail.com>
2023-05-09 19:34:20 +0200

    Read *.pem and print N in decimal

--- corsair.c
+++ corsair.c
@@ -6,61 +6,86 @@
 /*   By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 12:10:54 by rzamolo-          #+#    #+#             */
-/*   Updated: 2023/05/08 17:35:10 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:21:27 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corsair.h"
 
+// Buscar as Funcoes
+// ERR_load_bio_strings
+// ERR_load_crypto_strings
+// BIO_new_file
+// PEM_read_bio_PUBKEY
+// EVP_PKEY_get1_RSA
+// RSA_get0_key
+// BN_bn2hex
+
+void specific_function(const char* filename) {
+    // Implement your specific function to process each file here
+    read_public_key(filename);
+}
+
 
 void read_public_key(char *file)
 {
-    ERR_load_BIO_strings();
-    ERR_load_crypto_strings();
+	ERR_load_BIO_strings();
+	ERR_load_crypto_strings();
 
-    BIO *bio = NULL;
-    const char *filename = "public.pem";
-    bio = BIO_new_file(filename, "r");
-    if (bio == NULL) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
+	BIO *bio = NULL;
+	// const char *filename = "public.pem";
+	bio = BIO_new_file(file, "r");
+	if (bio == NULL) {
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	EVP_PKEY *evp_key = NULL;
+	evp_key = PEM_read_bio_PUBKEY(bio, NULL, 0, NULL);
+	if (evp_key == NULL) {
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	RSA *rsa_key = NULL;
+	rsa_key = EVP_PKEY_get1_RSA(evp_key);
+	if (rsa_key == NULL) {
+		ERR_print_errors_fp(stderr);
+		exit(EXIT_FAILURE);
+	}
+
+	const BIGNUM *n, *e;
+	RSA_get0_key(rsa_key, &n, &e, NULL);
+
+	char *n_hex = BN_bn2hex(n);
+	char *e_hex = BN_bn2hex(e);
+	
+	//
+	BN_CTX *ctx = BN_CTX_new();
+	char *dec_str = BN_bn2dec(n);
+	if (dec_str == NULL) {
+		printf("Error converting BIGNUM to decimal string\n");
     }
 
-    EVP_PKEY *evp_key = NULL;
-    evp_key = PEM_read_bio_PUBKEY(bio, NULL, 0, NULL);
-    if (evp_key == NULL) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
-    }
+    // Print the decimal string
+    printf("BIGNUM in decimal: %s\n", dec_str);
+	//
 
-    RSA *rsa_key = NULL;
-    rsa_key = EVP_PKEY_get1_RSA(evp_key);
-    if (rsa_key == NULL) {
-        ERR_print_errors_fp(stderr);
-        exit(EXIT_FAILURE);
-    }
+	printf("Public key (%s)\n", file);
+	printf("n: %s\n", n_hex);
+	printf("e: %s\n", e_hex);
 
-    const BIGNUM *n, *e;
-    RSA_get0_key(rsa_key, &n, &e, NULL);
+	OPENSSL_free(n_hex);
+	OPENSSL_free(e_hex);
 
-    char *n_hex = BN_bn2hex(n);
-    char *e_hex = BN_bn2hex(e);
-
-	printf("Public key\n");
-    printf("n: %s\n", n_hex);
-    printf("e: %s\n", e_hex);
-
-    OPENSSL_free(n_hex);
-    OPENSSL_free(e_hex);
-
-    RSA_free(rsa_key);
-    EVP_PKEY_free(evp_key);
-    BIO_free_all(bio);
-    ERR_free_strings();
+	RSA_free(rsa_key);
+	EVP_PKEY_free(evp_key);
+	BIO_free_all(bio);
+	ERR_free_strings();
 
 }
 
-void	read_private_key(char *file)
+void read_private_key(char *file)
 {
 	OpenSSL_add_all_algorithms();
 	ERR_load_BIO_strings();
@@ -127,11 +152,30 @@ void	read_private_key(char *file)
 	EVP_cleanup();
 	ERR_free_strings();
 }
+int main(void) {
+    const char *folder = "./challenge_corsair/";  // Set the path to the folder containing the PEM files
+    DIR *dir_ptr;                                  // Declare a directory stream
+    struct dirent *entry;                          // Declare a directory entry structure
+    
+    dir_ptr = opendir(folder);                     // Open the directory stream
 
-int main(void) 
-{
-	read_private_key("private.pem");
-	read_public_key("public.pem");
+    if (dir_ptr == NULL) {                         // Check if the directory stream was successfully opened
+        perror("Unable to read directory");        // Print error message and exit if not
+        return 1;
+    }
 
-	return (0);
+    while ((entry = readdir(dir_ptr)) != NULL) {   // Loop through the directory entries
+        if (entry->d_type == DT_REG) {             // Check if the entry is a regular file
+            char *ext = strrchr(entry->d_name, '.');  // Find the last occurrence of '.' in the filename to get the file extension
+            
+            if (ext != NULL && strcmp(ext, ".pem") == 0) {  // Check if the file extension is ".pem"
+                char filepath[512];               // Declare a buffer to store the full file path
+                snprintf(filepath, sizeof(filepath), "%s/%s", folder, entry->d_name);  // Construct the full file path
+                specific_function(filepath);      // Call the specific function with the full file path as argument
+            }
+        }
+    }
+
+    closedir(dir_ptr);                             // Close the directory stream
+    return 0;
 }
--- corsair.h
+++ corsair.h
@@ -6,37 +6,44 @@
 /*   By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/24 11:44:38 by rzamolo-          #+#    #+#             */
-/*   Updated: 2023/05/08 13:25:45 by rzamolo-         ###   ########.fr       */
+/*   Updated: 2023/05/09 19:17:31 by rzamolo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CORSAIR_H
-# define CORSAIR_H
+#define CORSAIR_H
 
-# include <stdio.h>
-# include <stdlib.h>
-# include <unistd.h>
-# include <fcntl.h>
+#include <dirent.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-# include <math.h>
-# include <string.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
 
-# include <openssl/aes.h>
-# include <openssl/bio.h>
-# include <openssl/bn.h>
+#include <math.h>
+#include <string.h>
 
-# include <openssl/crypto.h>
-# include <openssl/err.h>
-# include <openssl/evp.h>
+#include <openssl/aes.h>
+#include <openssl/bio.h>
+#include <openssl/bn.h>
 
-# include <openssl/objects.h>
-# include <openssl/pem.h>
-# include <openssl/pkcs12.h>
-# include <openssl/rsa.h>
+#include <openssl/crypto.h>
+#include <openssl/err.h>
+#include <openssl/evp.h>
 
-# include <openssl/ssl.h>
-# include <openssl/x509.h>
-# include <openssl/x509v3.h>
+#include <openssl/objects.h>
+#include <openssl/pem.h>
+#include <openssl/pkcs12.h>
+#include <openssl/rsa.h>
+
+#include <openssl/ssl.h>
+#include <openssl/x509.h>
+#include <openssl/x509v3.h>
+
+void specific_function(const char* filename);
+void read_public_key(char *file);
+void read_private_key(char *file);
 
 
 #endif
